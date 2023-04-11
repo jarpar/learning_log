2dd8a5536c0e39faa2502e17884f664f693b1ab8
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-11 14:41:22 +0200

    added app to settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Moje aplikacje
+    'learning_logs',
+    
+    #Domyślne aplikacje Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
