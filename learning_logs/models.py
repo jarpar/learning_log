8c0b5baefb0cde0c5e0b197f493dcc1469c3985c
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-11 14:58:50 +0200

    make migration

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Topic(models.Model):
-"""Temat poznawany przez użytkownika."""
-text = models.CharField(max_length=200)
-date_added = models.DateTimeField(auto_now_add=True)
+    """Temat poznawany przez użytkownika."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
-"""Zwraca reprezentację modelu w postaci ciągu tekstowego."""
-return self.text
+    """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
+    return self.text
 
