037affc1a7425fefb9535006b215785fcf4af058
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-12 17:47:38 +0200

    reformatted

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Topic(models.Model):
@@ -7,7 +8,7 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+
 def __str__(self):
     """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
     return self.text
-
